050c043a5dd659726b115619d33f477a5c3094c0
zhaolong01 <zhaolong01@shengpay.com>
2017-09-26 10:30:49 +0800

    初始化提交

---
+++ SJCirqueView/Classes/SJCirqueView.h
@@ -0,0 +1,13 @@
+//
+//  SJCirqueView.h
+//  Pods
+//
+//  Created by SDPMobile on 2017/9/25.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SJCirqueView : UIView
+
+@end
---
+++ SJCirqueView/Classes/SJCirqueView.m
@@ -0,0 +1,21 @@
+//
+//  SJCirqueView.m
+//  Pods
+//
+//  Created by SDPMobile on 2017/9/25.
+//
+//
+
+#import "SJCirqueView.h"
+
+@implementation SJCirqueView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
